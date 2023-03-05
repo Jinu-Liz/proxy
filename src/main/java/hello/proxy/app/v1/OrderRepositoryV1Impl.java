190368d7d47d941929fdfea87d063a2a253e33f5
Hyungseock-Yun <bagzein@gmail.com>
2023-03-05 23:36:34 +0900

    Initial commit

---
+++ src/main/java/hello/proxy/app/v1/OrderRepositoryV1Impl.java
@@ -0,0 +1,22 @@
+package hello.proxy.app.v1;
+
+public class OrderRepositoryV1Impl implements OrderRepositoryV1 {
+
+  @Override
+  public void save(String itemId) {
+    // 저장 로직
+    if (itemId.equals("ex")) {
+      throw new IllegalStateException("예외 발생!");
+    }
+
+    sleep(1000);
+  }
+
+  private void sleep(int time) {
+    try {
+      Thread.sleep(time);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
