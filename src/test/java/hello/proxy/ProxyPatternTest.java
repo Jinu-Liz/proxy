86dddd892b6a97b7c2aad79c2b449efb026b8ed6
Hyungseock-Yun <bagzein@gmail.com>
2023-03-07 22:45:46 +0900

    - 프록시 패턴 CacheProxy 사용

--- src/test/java/hello/proxy/ProxyPatternTest.java
+++ src/test/java/hello/proxy/ProxyPatternTest.java
@@ -1,5 +1,6 @@
 package hello.proxy;
 
+import hello.proxy.code.CacheProxy;
 import hello.proxy.code.ProxyPatternClient;
 import hello.proxy.code.RealSubject;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,17 @@ public class ProxyPatternTest {
   void noProxyTest() {
     RealSubject realSubject = new RealSubject();
     ProxyPatternClient client = new ProxyPatternClient(realSubject);
+    client.execute();
+    client.execute();
+    client.execute();
+  }
+
+  @Test
+  void cacheProxyTest() {
+    RealSubject realSubject = new RealSubject();
+    CacheProxy cacheProxy = new CacheProxy(realSubject);
+    ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
+
     client.execute();
     client.execute();
     client.execute();
