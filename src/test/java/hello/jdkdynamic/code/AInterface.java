57b0bba43021d9963a13e5ee35e9205ab27c2062
Hyungseock-Yun <bagzein@gmail.com>
2023-03-10 03:05:23 +0900

    - JDK 동적 프록시

---
+++ src/test/java/hello/jdkdynamic/code/AInterface.java
@@ -0,0 +1,5 @@
+package hello.jdkdynamic.code;
+
+public interface AInterface {
+  String call();
+}
---
+++ src/test/java/hello/jdkdynamic/code/BInterface.java
@@ -0,0 +1,5 @@
+package hello.jdkdynamic.code;
+
+public interface BInterface {
+  String call();
+}
